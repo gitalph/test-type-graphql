2f93e2de86e5f4ce8307d5336cd1640f26a5dfe6
alph <capocr@narod.ru>
2020-04-08 21:47:01 +0300

    start

--- app.ts
+++ app.ts
@@ -1,3 +1,28 @@
-export {};
+import "reflect-metadata";
+import { createConnection } from "typeorm";
+import { ApolloServer } from "apollo-server";
+import { buildSchema } from "type-graphql";
 
-console.log("Hello from docker");
+import { BookResolver } from "./resolvers/BookResolver";
+
+async function main() {
+  await createConnection({
+      "type": "mysql",
+      "host": "db",
+      "port": 3306,
+      "username": "test",
+      "password": "test",
+      "database": "test",
+      "synchronize": true,
+      "logging": false,
+      "entities": [
+        __dirname + '/models{.ts,.js}'
+      ]
+  });
+  const schema = await buildSchema({ resolvers: [BookResolver] });
+  const server = new ApolloServer({ schema });
+  await server.listen(4000);
+  console.log("Server has started!");
+}
+
+main();
---
+++ resolvers/BookResolver.ts
@@ -0,0 +1,24 @@
+import { Resolver, Query, Mutation, Arg } from "type-graphql";
+import { Book } from "../models/Book";
+import { CreateBookInput } from "../inputs/CreateBookInput";
+
+@Resolver()
+export class BookResolver {
+  @Query(() => [Book])
+  books() {
+    return Book.find();
+  }
+
+  @Query(() => Book)
+  book(@Arg("bookId") bookId: string) {
+    return Book.findOne({ where: { bookId } });
+  }
+
+  @Mutation(() => Book)
+  async createBook(@Arg("data") data: CreateBookInput) {
+    const book = Book.create(data);
+    await book.save();
+    return book;
+  }
+
+}
